d65a1ee87d1762a89d1b6ea36fb6037c5c059c50
pegy0210 <pegy0210@gmail.com>
2026-08-08 16:31:38 +0800

    Defer MainRoom scene change during boot

--- game/scripts/boot/boot.gd
+++ game/scripts/boot/boot.gd
@@ -6,4 +6,8 @@ func _ready() -> void:
 	EconomyService.recalculate()
 	OfflineService.prepare_pending_reward()
 	EconomyService.start_runtime()
+	call_deferred("_enter_main_room")
+
+
+func _enter_main_room() -> void:
 	get_tree().change_scene_to_file("res://game/scenes/main_room/main_room.tscn")
